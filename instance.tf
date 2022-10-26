6cdacf70cb40b1ec284882ece2c8d0ec3f08c0b2
SirRobZ <sazarapkar@gmail.com>
2022-10-26 13:44:04 -0400

    added user-data to install apache webserver on public ec2 instance

--- instance.tf
+++ instance.tf
@@ -23,6 +23,8 @@ resource "aws_instance" "public" {
   vpc_security_group_ids      = [aws_security_group.public.id]
   subnet_id                   = aws_subnet.public[0].id
 
+  user_data = file("user-data.sh")
+
   tags = {
     Name = "${var.env_code}-public"
   }
@@ -49,6 +51,14 @@ resource "aws_security_group" "public" {
     description = "SSH from VPC"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["71.140.147.152/32"]
+  }
+
+  ingress {
+    description = "HTTP from public"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["71.140.147.152/32"]
   }
