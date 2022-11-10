383c62bff10d5cd42181075802bc674b87ce0393
SirRobZ <sazarapkar@gmail.com>
2022-11-10 11:06:52 -0500

    fixed formatting

--- level2/instance.tf
+++ level2/instance.tf
@@ -90,9 +90,9 @@ resource "aws_security_group" "private" {
 
   ingress {
     description = "HTTP from load balancer"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
     security_groups = [aws_security_group.load_balancer.id]
   }
 
